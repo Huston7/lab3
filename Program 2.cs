a8335e5c1f55b74e9c639a42202305d64dc1ce0c
Huston7 <144244378+Huston7@users.noreply.github.com>
2023-10-01 16:10:39 +0300

    Add files via upload

---
+++ Program 2.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+class Point
+{
+    public double X { get; }
+    public double Y { get; }
+    public string Name { get; }
+
+    public Point(double x, double y, string name)
+    {
+        X = x;
+        Y = y;
+        Name = name;
+    }
+}
+
+class Figure
+{
+    private Point[] points;
+
+    public Figure(Point p1, Point p2, Point p3)
+    {
+        points = new Point[] { p1, p2, p3 };
+    }
+
+    public Figure(Point p1, Point p2, Point p3, Point p4)
+    {
+        points = new Point[] { p1, p2, p3, p4 };
+    }
+
+    public Figure(Point p1, Point p2, Point p3, Point p4, Point p5)
+    {
+        points = new Point[] { p1, p2, p3, p4, p5 };
+    }
+
+    public double GetSideLength(Point A, Point B)
+    {
+        return Math.Sqrt(Math.Pow(B.X - A.X, 2) + Math.Pow(B.Y - A.Y, 2));
+    }
+
+    public void CalculatePerimeter()
+    {
+        double perimeter = 0;
+        for (int i = 0; i < points.Length; i++)
+        {
+            int nextIndex = (i + 1) % points.Length;
+            perimeter += GetSideLength(points[i], points[nextIndex]);
+        }
+        Console.WriteLine($"Периметр багатокутника: {perimeter}");
+    }
+}
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Point p1 = new Point(0, 0, "A");
+        Point p2 = new Point(4, 0, "B");
+        Point p3 = new Point(0, 3, "C");
+
+        Figure figure = new Figure(p1, p2, p3);
+        figure.CalculatePerimeter();
+    }
+}
